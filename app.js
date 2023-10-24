3db74e374a86bb069eea1076543b2196b444541e
amitgarg0403 <amitgarg0403@gmail.com>
2023-10-24 18:01:37 +0530

    Complete Basic Blog website

--- app.js
+++ app.js
@@ -1,29 +1,68 @@
 // Require Package & Module
 const express = require("express");
-
-
-const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const PORT = 5000;
+const Schema = mongoose.Schema;
+const app = express();
 
 // Middlewares
-app.set('view engine', "ejs")
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 //Static Folder 'Public
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.get("/", function(req,res){
-    res.render("Home")
-})
+// MongoDB Connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/blogDB")
+  .then(console.log("MongoDb Connected"))
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.get('/compose', function(req,res){
-    res.render('Compose')
-})
+//Mongoose Schema
+const blogSchema = new Schema(
+  { title: String, category: String, description: String },
+  { timestamps: true }
+);
 
-app.get('/blogs', function(req,res){
-    res.render('blog')
+//Mongoose Mondel
+const Blog = mongoose.model("Blog", blogSchema);
+
+// Get Method
+app.get("/", function (req, res) {
+  res.render("Home");
+});
+
+app.get("/compose", function (req, res) {
+  res.render("Compose");
+});
+
+app.post("/compose", function (req, res) {
+  const post = new Blog({
+    title: req.body.postTitle,
+    category: req.body.category,
+    description: req.body.postDes,
+  });
+  post.save();
+  res.redirect("/compose");
+});
+
+app.get("/blogs", async function (req, res) {
+  let Post = await Blog.find();
+  res.render("Blog",{ POST: Post});
+});
+
+app.post("/delete", (req,res)=>{
+    let d = req.body.deleteItem;
+    Blog.findByIdAndDelete(d)
+    .then(console.log("Deleted Post"))
+    .catch((err) =>{console.log(err)})
+    res.redirect("/blogs")
 })
 
 // Listening Port
-app.listen(PORT, function(req,res){
-    console.log("Server Ruuning at " + PORT);
-})
+app.listen(PORT, function (req, res) {
+  console.log("Server Ruuning at " + PORT);
+});
